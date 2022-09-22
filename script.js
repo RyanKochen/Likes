27c3c57045abaab39de20b9ef656e6763f008631
RyanKochen <rkochen@comcast.net>
2022-09-22 10:40:04 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function plusNeil() {
+    let nmLikes = document.querySelector('#nmLikes');
+    nmLikes.innerText++;
+};
+
+function plusNichole() {
+    let nkLikes = document.querySelector('#nkLikes');
+    nkLikes.innerText++;
+};
+
+function plusJim() {
+    let jrLikes = document.querySelector('#jrLikes');
+    jrLikes.innerText++;
+};
